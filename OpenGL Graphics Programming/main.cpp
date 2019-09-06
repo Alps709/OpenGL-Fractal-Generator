a646e083a06ed200e809ffe198281de782222b81
Anton <alps709@gmail.com>
2019-09-06 17:59:32 +1200

    Actually added mandlebrot function.

--- OpenGL Graphics Programming/main.cpp
+++ OpenGL Graphics Programming/main.cpp
@@ -2,6 +2,7 @@
 #include <freeglut.h>
 #include <iostream>
 #include <vector>
+#include <complex>
 //#include "vld.h"
 
 #include "Utils.h"
@@ -116,6 +117,60 @@ void Update()
 	u_currentTime = u_currentTime * 0.001f; //Convert milliseconds to seconds
 	glutPostRedisplay();
 	//ProcessInput();
+}
+
+// Render the Mandelbrot set into the image array.
+// The parameters specify the region on the complex plane to plot.
+void compute_mandelbrot(double left, double right, double top, double bottom)
+{
+	// The number of times to iterate before we assume that a point isn't in the
+	// Mandelbrot set.
+	// (You may need to turn this up if you zoom further into the set.)
+	const int MAX_ITERATIONS = 500;
+
+	const int width = glutGet(GLUT_WINDOW_WIDTH);
+	const int height = glutGet(GLUT_WINDOW_HEIGHT);
+
+	glBegin(GL_POINTS); // start drawing in single pixel mode
+	for (int y = 0; y < height; ++y)
+	{
+		for (int x = 0; x < width; ++x)
+		{
+			// Work out the point in the complex plane that
+			// corresponds to this pixel in the output image.
+			std::complex<long long> c(left + (x * (right - left) / width),
+				top + (y * (bottom - top) / height));
+
+			// Start off z at (0, 0).
+			std::complex<long long> z(0.0, 0.0);
+
+			// Iterate z = z^2 + c until z moves more than 2 units
+			// away from (0, 0), or we've iterated too many times.
+			int iterations = 0;
+			while (abs(z) < 2.0 && iterations < MAX_ITERATIONS)
+			{
+				z = (z * z) + c;
+
+				++iterations;
+			}
+
+			if (iterations == MAX_ITERATIONS)
+			{
+				glColor3f(1.0, 0.0, 0.0); // Set color to draw mandelbrot
+				// z didn't escape from the circle.
+				// This point is in the Mandelbrot set.
+				glVertex2i(x, y);
+			}
+			else
+			{
+				glColor3f(0.0, 0.0, 0.0); //Set pixel to black
+				// z escaped within less than MAX_ITERATIONS
+				// iterations. This point isn't in the set.
+				glVertex2i(x, y);
+			}
+		}
+	}
+	glEnd();
 }
 
 //void ProcessInput()
